b9d3f58f61bcf263d995860c9e347cfb20da1b55
kimhoon <kimhoon0316@gmail.com>
2021-06-02 18:48:20 +0900

    lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename).expect("file not found");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)?;
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,32 +18,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-}
+    if let Err(e) = greprs::run(config) {
+        println!("Application error: {}", e);
 
-fn run(config: Config) {
-    let mut f = File::open(config.filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        })
+        process::exit(1);
     }
 }
+
